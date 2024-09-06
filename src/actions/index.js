f8c9f6d5c6279bf7d19fdfa95eeee3a17ad07341
Alexander0908 <sanya090895@gmail.com>
2024-09-06 16:41:54 +0300

    updated HeroesFilters
    updated HeroesFilters

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,13 +23,36 @@ export const filtersFetching = () => {
     }
 }
 
+export const filtersFetched = (filters) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: filters
+    }
+}
 
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
 
-
+export const activeFilterChanged = (filter) => {
+    return {
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
+}
 
 export const heroCreated = (hero) => {
     return {
         type: 'HERO_CREATED',
         payload: hero
+    }
+}
+
+export const heroDeleted = (id) => {
+    return {
+        type: 'HERO_DELETED',
+        payload: id
     }
 }

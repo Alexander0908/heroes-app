7cbf37c11b7493240710dc0e74a5a65f61b11f46
Alexander0908 <sanya090895@gmail.com>
2024-11-22 12:58:29 +0200

    updated index.js with configureStore
    updated index.js with configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import {thunk} from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
@@ -27,14 +28,20 @@ const stringMiddleware = () => (next) => (action) => {
 //     return store;
 // }
 
-const store = createStore( 
-                    combineReducers({heroes, filters}), 
-                    compose(applyMiddleware(thunk, stringMiddleware),
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-                // compose(
-                //     enhancer,
-                //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                // )
-                );
+// const store = createStore( 
+//                     combineReducers({heroes, filters}), 
+//                     compose(applyMiddleware(thunk, stringMiddleware),
+//                     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//                 // compose(
+//                 //     enhancer,
+//                 //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//                 // )
+//                 );
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
